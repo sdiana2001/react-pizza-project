03b647a478d2ec8a9ace1bb11d87dded981dba4e
Sogdiana <sogdianaakmatova4@gmail.com>
2026-07-27 15:14:51 +0600

    feat: add skeleton loaders for pizza cards during loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,63 +3,28 @@ import Header from './assets/components/Header/Header';
 import Categories from './assets/components/Categories/Categories';
 import Sort from './assets/components/Sort/Sort';
 import PizzaBlock from './assets/components/PizzaBlock/PizzaBlock';
-import { useState } from 'react';
-
-const data = [
-  {
-    id: 0,
-    imageUrl: '/pizza/pizza-1.png',
-    title: 'Чизбургер-пицца',
-    types: [0, 1],
-    sizes: [26, 30, 40],
-    price: 395,
-    category: 0,
-    rating: 4,
-  },
-  {
-    id: 1,
-    imageUrl: '/pizza/pizza-2.png',
-    title: 'Сырная',
-    types: [0, 1],
-    sizes: [26],
-    price: 450,
-    category: 1,
-    rating: 5,
-  },
-  {
-    id: 2,
-    imageUrl: '/pizza/pizza-3.png',
-    title: 'Креветки по-азиатски',
-    types: [0],
-    sizes: [30],
-    price: 290,
-    category: 2,
-    rating: 3,
-  },
-  {
-    id: 3,
-    imageUrl: '/pizza/pizza-4.png',
-    title: 'Сырный цыпленок',
-    types: [0, 1],
-    sizes: [26, 30, 40],
-    price: 385,
-    category: 3,
-    rating: 5,
-  },
-];
-
+import { useEffect, useState } from 'react';
+import Skeleton from './assets/components/PizzaBlock/Skeleton'
 
 function App() {
-  const [pizzaItem, setPizzaItem] = useState([]);
-   console.log(pizzaItem);
+  const [pizzaItems, setPizzaItem] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    fetch('https://66a904f6e40d3aa6ff5a4dc3.mockapi.io/item')
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setPizzaItem(res);
+        setIsLoading(false);
+      });
+  }, []);
 
   // const onClickAdd=(obj)=>{
   //  setPizzaItem((prev)=> [...prev, obj])
   //  console.log(pizzaItem);
   // }
-
- 
 
   return (
     <div className="wrapper">
@@ -72,9 +37,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {data.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : pizzaItems.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
---
+++ src/assets/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,23 @@
+import ContentLoader from 'react-content-loader';
+
+const Skeleton = (props) => (
+  <ContentLoader
+    className="pizza-block"
+    speed={2}
+    width={280}
+    height={465}
+    viewBox="0 0 280 465"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}>
+    <circle cx="134" cy="114" r="98" />
+    <rect x="202" y="287" rx="0" ry="0" width="17" height="1" />
+    <rect x="6" y="231" rx="13" ry="13" width="265" height="27" />
+    <rect x="4" y="277" rx="15" ry="15" width="269" height="96" />
+    <rect x="8" y="401" rx="14" ry="14" width="94" height="32" />
+    <rect x="113" y="442" rx="0" ry="0" width="2" height="9" />
+    <rect x="128" y="392" rx="23" ry="23" width="145" height="49" />
+  </ContentLoader>
+);
+
+export default Skeleton;
